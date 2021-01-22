52ad9c200dba2e334a86d6fce1da6aa821d9d257
qxz0tw0 <werner.grobler@partner.bmw.co.za>
2021-01-22 23:56:32 +0200

    Switch to python and Flask

---
+++ sortApi/api.py
@@ -0,0 +1,26 @@
+import flask
+from flask import request, jsonify
+import bubbleSort
+from bubbleSort import bubbleSort
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>This is the home page of my application</h1> <a href=\"/api/bubbleSort\">Go here to see bubble sort</a>"
+
+@app.route('/api/bubbleSort', methods=['GET', 'POST'])
+def api_array():
+    # if 'array' in request.args:
+    #     myArray = list(map(int, request.args['array'].split(",")))
+    #     result = bubbleSort(myArray)
+    # else:
+    #     return "Not calling sort: add ?array=5,4,3,2,1 as paramater to test"
+
+    # return str(result)
+    content = request.get_json()
+    return content
+
+
+
+app.run()
---
+++ sortApi/curlSort.sh
@@ -0,0 +1,4 @@
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"array":"[1,4,5,2,3]"}' \
+  http://localhost:5000/api/bubbleSort
