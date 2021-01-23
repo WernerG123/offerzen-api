66c577e5733354ff8eb7a120b2381af1585a6885
qxz0tw0 <werner.grobler@partner.bmw.co.za>
2021-01-24 01:25:24 +0200

    Removed proxy curl requests

--- sortApi/curlSort.sh
+++ sortApi/curlSort.sh
@@ -2,7 +2,3 @@
 curl --header "Content-Type: application/json" --request POST --data '{"array":"[1,4,5,2,3, [7,6], 8]"}' http://localhost:5000/api/bubbleSort
 curl --header "Content-Type: application/json" --request POST --data '{"array":"[6,2,[4,3],[[[5],7], 1]]"}' http://localhost:5000/api/bubbleSort
 curl --header "Content-Type: application/json" --request POST --data '{"array":"[[6,2,[4,3],[[5,8],7], 1]]"}' http://localhost:5000/api/bubbleSort
-
-curl --header "Content-Type: application/json" --request POST --data '{"array":"[1,4,5,2,3, [7,6], 8]"}' http://taipsiciw1.bmwgroup.net/:5000/api/bubbleSort
-curl --header "Content-Type: application/json" --request POST --data '{"array":"[6,2,[4,3],[[[5],7], 1]]"}' http://taipsiciw1.bmwgroup.net/:5000/api/bubbleSort
-curl --header "Content-Type: application/json" --request POST --data '{"array":"[[6,2,[4,3],[[5,8],7], 1]]"}' http://taipsiciw1.bmwgroup.net/:5000/api/bubbleSort
